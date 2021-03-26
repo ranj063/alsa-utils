8b541bd9f01646fab8376cdbd936bfbf50620af9
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2021-03-26 14:44:13 -0700

    topology: Add support for pre-processing Topology2.0 syntax
    This patch adds support for pre-processing the Topology2.0.
    The '-p' switch add pre-processing support during compilation
    and the '-P' switch is for converting the Topology2.0
    configuration file into the existing syntax.
    
    Topology2.0 is a high level keyword extension on top of the existing ALSA
    conf topology format designed to:
    
    1) Simplify the ALSA conf topology definitions by providing high level
       "classes" so topology designers need to write less config for common
       object definitions.
    
    2) Allow simple reuse of objects. Define once and reuse (like M4) with
       the ability to alter objects configuration attributes from defaults.
    
    3) Allow data type and value verification. This is not done today and
       frequently crops up in FW bug reports.
    
    Common Topology Classes
    -----------------------
    
    Topology today has some common classes that are often reused throughout
    with slightly altered configurations. i.e. widgets (components),
    pipelines, dais and controls.
    
    Topology2.0 introduces the high level concept of reusable "class" like
    definition for that can be used to create topology objects.
    
    Common Topology Attributes
    --------------------------
    Topology defines a lot of attributes per object with different types
    and constraints. Today there is no easy way to validate type or
    constraints and this can lead to many hard to find problems in FW at
    runtime.
    
    A new keyword "DefineAttribute" has been added to define attribute
    constraints such as min value, max value, enum_values etc. This
    then allows alsatplg to validate each topology object attribute.
    
    Topology Classes define the list of attributes that they use and
    whether the attribute is mandatory, can be overridden by parent users
    or is immutable. This also helps alsatplg emit the appropriate errors
    for attribute misuse.
    
    Common Topology Arguments
    -------------------------
    
    Arguments are used to pass parameters needed for instantiating an
    object and construcinting the object name. A new keyword
    "DefineArgument" has been added to define the arguments. The order in
    which the arguments are defined determines the name for the widget.
    For example, in the case of the host widget, the name would be
    constructed as "host.1.playback" where "1" is the pipeline_id argument
    value and "playback" is the direction argument value.
    
    Attribute Inheritance:
    ----------------------
    One of the key features of Topology2.0 is how the attribute values are
    propagated from a parent object to a child object. For ex: a pipeline
    object can pass down the pipeline_id attribute to all its widgets.
    Inheritance is implicit when an object and its embedded child objects
    have matching names for a attribute/argument. Attribute values
    set explicitly in an object instance always has precedence over
    the values inherited from the parent object.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    
    1
    
    1
    
    1

---
+++ topology/pre-processor.c
@@ -0,0 +1,207 @@
+/*
+  Copyright(c) 2021 Intel Corporation
+  All rights reserved.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+  The full GNU General Public License is included in this distribution
+  in the file called LICENSE.GPL.
+*/
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+
+#include <alsa/input.h>
+#include <alsa/output.h>
+#include <alsa/conf.h>
+#include "gettext.h"
+#include "topology.h"
+
+/*
+ * Parse compound config nodes
+ */
+static int pre_process_compound(struct tplg_pre_processor *tplg_pp, snd_config_t *cfg,
+				int (*fcn)(struct tplg_pre_processor *, snd_config_t *))
+{
+	const char *id;
+	snd_config_iterator_t i, next;
+	snd_config_t *n;
+	int err = -EINVAL;
+
+	if (snd_config_get_id(cfg, &id) < 0)
+		return -EINVAL;
+
+	if (snd_config_get_type(cfg) != SND_CONFIG_TYPE_COMPOUND) {
+		fprintf(stderr, "compound type expected for %s", id);
+		return -EINVAL;
+	}
+
+	/* parse compound */
+	snd_config_for_each(i, next, cfg) {
+		n = snd_config_iterator_entry(i);
+
+		if (snd_config_get_type(cfg) != SND_CONFIG_TYPE_COMPOUND) {
+			fprintf(stderr, _("compound type expected for %s, is %d"),
+				id, snd_config_get_type(cfg));
+			return -EINVAL;
+		}
+
+		err = fcn(tplg_pp, n);
+		if (err < 0)
+			return err;
+	}
+
+	return err;
+}
+
+static int pre_process_config(struct tplg_pre_processor *tplg_pp, snd_config_t *cfg)
+{
+	int (*parser)(struct tplg_pre_processor *tplg_pp, snd_config_t *cfg);
+	snd_config_iterator_t i, next;
+	snd_config_t *n;
+	const char *id;
+	int err;
+
+	if (snd_config_get_type(cfg) != SND_CONFIG_TYPE_COMPOUND) {
+		fprintf(stderr, "compound type expected at top level");
+		return -EINVAL;
+	}
+
+	/* parse topology config sections */
+	snd_config_for_each(i, next, cfg) {
+		n = snd_config_iterator_entry(i);
+		if (snd_config_get_id(n, &id) < 0)
+			continue;
+
+		parser = NULL;
+		if (!strcmp(id, "Class")) {
+			/* TODO: set object parser */
+		}
+
+		if (!strcmp(id, "Object")) {
+			/* TODO: set object parser */
+		}
+
+		if (!parser)
+			continue;
+
+		err = pre_process_compound(tplg_pp, n, parser);
+		if (err < 0)
+			return err;
+
+	}
+
+	return 0;
+}
+
+void free_pre_preprocessor(struct tplg_pre_processor *tplg_pp)
+{
+	snd_output_close(tplg_pp->output);
+	snd_output_close(tplg_pp->dbg_output);
+	snd_config_delete(tplg_pp->cfg);
+	free(tplg_pp);
+}
+
+int init_pre_precessor(struct tplg_pre_processor **tplg_pp, snd_output_type_t type,
+		       const char *output_file)
+{
+	struct tplg_pre_processor *_tplg_pp;
+	int ret;
+
+	_tplg_pp = calloc(1, sizeof(struct tplg_pre_processor));
+	if (!_tplg_pp)
+		ret = -ENOMEM;
+
+	*tplg_pp = _tplg_pp;
+	INIT_LIST_HEAD(&_tplg_pp->class_list);
+	INIT_LIST_HEAD(&_tplg_pp->object_list);
+
+	/* create output top-level config node */
+	ret = snd_config_top(&_tplg_pp->cfg);
+	if (ret < 0)
+		goto err;
+
+	/* open output based of type */
+	if (type == SND_OUTPUT_STDIO) {
+		ret = snd_output_stdio_open(&_tplg_pp->output, output_file, "w");
+		if (ret < 0) {
+			fprintf(stderr, "failed to open file output\n");
+			goto open_err;
+		}
+	} else {
+		ret = snd_output_buffer_open(&_tplg_pp->output);
+		if (ret < 0) {
+			fprintf(stderr, "failed to open buffer output\n");
+			goto open_err;
+		}
+	}
+
+	/* debug output */
+	ret = snd_output_stdio_attach(&_tplg_pp->dbg_output, stdout, 0);
+	if (ret < 0) {
+		fprintf(stderr, "failed to open stdout output\n");
+		goto out_close;
+	}
+
+	return 0;
+out_close:
+	snd_output_close(_tplg_pp->output);
+open_err:
+	snd_config_delete(_tplg_pp->cfg);
+err:
+	free(_tplg_pp);
+	return ret;
+}
+
+int pre_process(struct tplg_pre_processor *tplg_pp, char *config, size_t config_size)
+{
+	snd_input_t *in;
+	snd_config_t *top;
+	int err;
+
+	/* create input buffer */
+	err = snd_input_buffer_open(&in, config, config_size);
+	if (err < 0) {
+		fprintf(stderr, "Unable to open input buffer\n");
+		return err;
+	}
+
+	/* create top-level config node */
+	err = snd_config_top(&top);
+	if (err < 0)
+		goto input_close;
+
+	/* load config */
+	err = snd_config_load(top, in);
+	if (err < 0) {
+		fprintf(stderr, "Unable not load configuration\n");
+		goto err;
+	}
+
+	err = pre_process_config(tplg_pp, top);
+	if (err < 0)
+		fprintf(stderr, "Unable to pre-process configuration\n");
+
+	/* save config to output */
+	err = snd_config_save(tplg_pp->cfg, tplg_pp->output);
+	if (err < 0)
+		fprintf(stderr, "failed to save pre-processed output file\n");
+
+err:
+	snd_config_delete(top);
+input_close:
+	snd_input_close(in);
+
+	return err;
+}
