576aa0427b4353b8aef34e8238d434330cc20ade
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2021-04-26 13:07:00 -0700

    topology: pre-process-dapm: add support for route objects
    DAPM route objects such as:
    Object.Base.route."1" {
    	source	"dai.SSP.0.dai.capture"
    	sink	"buffer.2.1"
    }
    
    will be converted to:
    
    SectionGraph."Endpoint.route.1" {
    	index 0
    	lines [
    		"dai.SSP.0.capture, , buffer.2.1"
    	]
    }
    
    If the source/sink names are references to objects within a parent pipeline
    object, the index attribute value can be skipped and it will be
    populated when the object is pre-processed
    
    Object.Pipeline.volume-capture."1" {
    	Object.Base.route."1" {
    		source	"pga..0"
    		sink	"buffer..0"
    	}
    }
    
    The reference pga..0 will need to be resolved to
    get the widget name pga.1.0 and buffer..0 will
    be resolved to buffer.1.0 before creating the SectionGraph as follows:
    
    SectionGraph."volume-capture.1.route.1" {
    	index 2
    	lines [
    		"pga.1.0, , buffer.1.0"
    	]
    }
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- topology/pre-process-object.c
+++ topology/pre-process-object.c
@@ -662,6 +662,7 @@ const struct build_function_map object_build_map[] = {
 	{"Base", "channel", "channel", &tplg_build_channel_object,
 	 &tplg_create_channel_template},
 	{"Base", "VendorToken", "SectionVendorTokens", &tplg_build_vendor_token_object, NULL},
+	{"Base", "route", "SectionGraph", &tplg_build_dapm_route_object, NULL},
 	{"Widget", "", "SectionWidget", &tplg_build_generic_object,
 	 &tplg_create_widget_config_template},
 	{"Control", "mixer", "SectionControlMixer", &tplg_build_mixer_control,
